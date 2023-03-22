81d84359fe7ba921eabea6dfaf400ce36955aad0
Yusuboy <yuusufab@outlook.com>
2023-03-23 00:58:42 +0200

    Tehtävä 6: tehtiin vs codeen uusia testejä

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 15.00 euroa")
+
+    def test_saldo_vähenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_syo_maukkaasti_Saldo_ei_muutu(self):
+        self.maksukortti.ota_rahaa(1500)
+        
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+
+    def test_rahat_riittivät(self):
+        self.maksukortti = Maksukortti(500)
+
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True) 
+        self.assertEqual(self.maksukortti.ota_rahaa(501), False)
+
+
+
