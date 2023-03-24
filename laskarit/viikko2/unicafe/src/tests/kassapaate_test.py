79ace477fe866390e2335b76adbd680d8189c916
Yusuboy <yuusufab@outlook.com>
2023-03-24 14:01:41 +0200

    Huomasin, että juuri hakemistossa oli asioita, joita ei oltu comitattu

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -22,7 +22,7 @@ class TestKassapääte(unittest.TestCase):
         kassa = Kassapaate()
         maksu = 100 
         vaihtoraha = kassa.syo_edullisesti_kateisella(maksu)
-        self.assertEqual(vaihtoraha, 100)  
+        self.assertEqual(vaihtoraha, 100, "Ei riittävästi käteistä")  
         self.assertEqual(kassa.kassassa_rahaa, 100000)  
         self.assertEqual(kassa.edulliset, 0) 
 
@@ -40,7 +40,7 @@ class TestKassapääte(unittest.TestCase):
         kassa = Kassapaate()
         maksu = 100 
         vaihtoraha = kassa.syo_edullisesti_kateisella(maksu)
-        self.assertEqual(vaihtoraha, 100)  
+        self.assertEqual(vaihtoraha, 100 , "Ei riittävästi käteistä")  
         self.assertEqual(kassa.kassassa_rahaa, 100000)  
         self.assertEqual(kassa.maukkaat, 0) 
 
@@ -51,10 +51,16 @@ class TestKassapääte(unittest.TestCase):
 
         asetus = kassapaate.syo_edullisesti_kortilla(kortti)
 
-        self.assertEqual(asetus, True)
+        self.assertEqual(asetus, True, "Kate oli riittävä")
         self.assertEqual(kortti.saldo, 760)
-        self.assertEqual(kassapaate.edulliset, 1)
         self.assertEqual(kassapaate.kassassa_rahaa, 100000)
+
+     def test_syo_edullisesti_kortilla_veloitus_onnistuu_määrä_kasvaa(self):
+        kortti = Maksukortti(1000)
+        kassapaate = Kassapaate()
+        asetus = kassapaate.syo_edullisesti_kortilla(kortti)
+        self.assertEqual(kassapaate.edulliset, 1, "Myytin ateria")
+        
 
      def test_syo_edullisesti_kortilla_veloitus_epäonnistuu(self):
         kortti = Maksukortti(100)
@@ -62,7 +68,7 @@ class TestKassapääte(unittest.TestCase):
 
         asetus = kassapaate.syo_edullisesti_kortilla(kortti)
 
-        self.assertEqual(asetus, False)
+        self.assertEqual(asetus, False, "Kate ei ollut riittävä")
         self.assertEqual(kortti.saldo, 100)
         self.assertEqual(kassapaate.edulliset, 0)
         self.assertEqual(kassapaate.kassassa_rahaa, 100000)
@@ -74,10 +80,16 @@ class TestKassapääte(unittest.TestCase):
 
         asetus = kassapaate.syo_maukkaasti_kortilla(kortti)
 
-        self.assertEqual(asetus, True)
+        self.assertEqual(asetus, True, "Kate oli riittävä")
         self.assertEqual(kortti.saldo, 600)
-        self.assertEqual(kassapaate.maukkaat, 1)
         self.assertEqual(kassapaate.kassassa_rahaa, 100000)
+
+     def test_syo_edullisesti_kortilla_veloitus_onnistuu_määrä_kasvaa(self):
+        kortti = Maksukortti(1000)
+        kassapaate = Kassapaate()
+        asetus = kassapaate.syo_edullisesti_kortilla(kortti)
+        self.assertEqual(kassapaate.edulliset, 1, "Myytiin ateria")
+        
 
 
      def test_syo_maukkaasti_kortilla_veloitus_epäonnistuu(self):
@@ -86,7 +98,7 @@ class TestKassapääte(unittest.TestCase):
 
         asetus = kassapaate.syo_maukkaasti_kortilla(kortti)
 
-        self.assertEqual(asetus, False)
+        self.assertEqual(asetus, False, "Kate ei ollut riittävä")
         self.assertEqual(kortti.saldo, 100)
         self.assertEqual(kassapaate.maukkaat, 0)
         self.assertEqual(kassapaate.kassassa_rahaa, 100000)
@@ -101,8 +113,20 @@ class TestKassapääte(unittest.TestCase):
         kassa.lataa_rahaa_kortille(kortti, 500)
         
      
-        self.assertEqual(kortti.saldo, 500)
-        self.assertEqual(kassa.kassassa_rahaa, 100500)
+        self.assertEqual(kortti.saldo, 500, "Kortin saldo muuttui")
+        self.assertEqual(kassa.kassassa_rahaa, 100500, "Kassan saldo muuttui")
+        
+        
+    
+
+     def test_neg_rahaa_ladattaessa_saldo_muuttuu(self):
+        kassa = Kassapaate()
+        kortti = Maksukortti(500)
+
+        kassa.lataa_rahaa_kortille(kortti, -100)
+
+      
+        self.assertEqual(kortti.saldo, 500 ,"Ei vaikutusta")
         
 
 
