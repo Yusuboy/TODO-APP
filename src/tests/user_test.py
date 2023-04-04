96cf86644c64d25df402823c3ae0fd851bd670f3
Yusuboy <yuusufab@outlook.com>
2023-04-04 19:37:02 +0300

    user tiedostolle tehtiin testit

---
+++ src/tests/user_test.py
@@ -0,0 +1,21 @@
+import unittest
+from entities.user import User
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User('Yuusuf', 'SQLlover')
+    # Test that shows if object exists
+    def test_user_exists(self):
+        self.assertNotEqual(self.user, None)
+    # test that shows username is correct    
+    def test_username_is_right(self):
+        self.assertEqual(self.user.username, 'Yuusuf')
+
+    # test that shows password is correct    
+    def test_password_is_right(self):
+        self.assertEqual(self.user.password, 'SQLlover')
+
+    
+
+
+
